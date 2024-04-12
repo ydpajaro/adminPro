caf41a2bdb6a53f25d158e06fb0ecf655aec8965
ydpajaro <yeiner.pajaro@udea.edu.co>
2024-04-12 10:11:33 -0500

    Se limpia app.module.ts con modulos apartes para la pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
