63689c6a33b9493dc50186cf64059632bb5f6c99
ydpajaro <yeiner.pajaro@udea.edu.co>
2024-04-12 09:29:27 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path:'', component:PagesComponent,
+   //Vistas protegidas
+    children:[
+      {path:'dashboard',component:DashboardComponent},
+      {path:'progress',component:ProgressComponent},
+      {path:'grafica',component:Grafica1Component},
+      {path:'',pathMatch:'full',redirectTo:'dashboard'},
+    ]
+  },
+  
+
+  //vistas públicas
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'**',component:NopagefoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
